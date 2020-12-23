7e47903aa51d03871cad52061c506e86fc5afbd3
yoo-mac <megaboy1129@naver.com>
2020-12-23 17:26:09 +0900

    source commit 20201223-001

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,16 @@ import { AppController } from './app.controller';
 import { ApiController } from './app.controller';
 import { AppService } from './app.service';
 import { ApiService } from './app.service';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'client'),
+      exclude: ['/api*'],
+    }),
+  ],
   controllers: [AppController, ApiController],
   providers: [AppService, ApiService],
 })
